856d6a3ca3513ef1f3decf7433fdbf6c6171ed95
Paul Cwalina <144741118+cwalinapj@users.noreply.github.com>
2026-02-06 22:04:21 -0800

    Export modules from index.ts in ddns-core

---
+++ ddns-core/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./normalize.js";
+export * from "./name_id.js";
+export * from "./crypto_ed25519.js";
+export * from "./routeset.js";
+export * from "./anchor.js";
+export * from "./bytes.js";
